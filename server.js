3d97b611c391e77495170578b4faaa02e79e0c19
MuhamadWahdan <104308780+MuhamadWahdan@users.noreply.github.com>
2024-01-17 19:26:32 +0100

    init frontend

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', (req, res) => {
     res.send('Project Parksystem');
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+
+app.get('/index', (req, res) => {
+    res.redirect('park1.html');
 });
+app.listen(port, () => console.log
+(`App listening on http://localhost:${port}`));
