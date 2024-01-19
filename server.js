c8fc2885e4489830407c292f6e74703ea5e40ad5
MuhamadWahdan <104308780+MuhamadWahdan@users.noreply.github.com>
2024-01-19 18:06:02 +0100

    json übergibt ein wert über frontend an Server

--- server.js
+++ server.js
@@ -1,17 +1,30 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.use(express.static('public'));
+
+/*app.get('/', (req, res) => {
+    res.send('Project Parksystem');
+});*/
 
 
 app.get('/', (req, res) => {
-    res.send('Project Parksystem');
+    const indexPath = path.join(__dirname, 'public', 'park1.html');
+    res.sendFile(indexPath);
 });
 
+app.post('/reservierung',(req,res) => {
+    console.log('JSON:', req.body);
+    res.send(req.body)
+})
 
-app.get('/index', (req, res) => {
-    res.redirect('park1.html');
-});
+app.get('/getReservierung',(req, res) => {
+    const indexPath = path.join(__dirname, 'public', 'reservierung.html');
+    res.redirect(indexPath);
+})
+
 app.listen(port, () => console.log
-(`App listening on http://localhost:${port}`));
+(`App listening on http://localhost:${port}`))
