a8db16c7385f5aa02bee8e217eb669095033e684
MuhamadWahdan <104308780+MuhamadWahdan@users.noreply.github.com>
2024-01-20 00:02:27 +0100

    Addit audit folder

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-const Parkhaus = require('./framework/Parkhaus.js'); // Passe den Pfad entsprechend an
-const Parkplatz = require('./framework/Parkplatz.js'); // Passe den Pfad entsprechend an
+const Parkhaus = require('./framework/Parkhaus.js'); 
 const DBManagement = require('./framework/DBManagement.js');
 
 const express = require('express');
@@ -9,21 +8,13 @@ const port = 3000;
 
 app.use(express.static('public'));
 
-/*app.get('/', (req, res) => {
-    res.send('Project Parksystem');
-});*/
 
-
-app.get('/index', (req, res) => {
-});
 
 //Alle Daten des Parkhaus
 app.get('/', (req, res) => {
 
     const DBManagementInstanz1 = new DBManagement();
     const DBJsonData = DBManagementInstanz1.jsonDateiAuslesen('DB.json');
-
-
     res.send(DBJsonData);
 
 })
@@ -31,13 +22,7 @@ app.get('/', (req, res) => {
 app.get('/update',(req,res) => {
     //hier wird das updaten der DB.json implimentiert
     const DBManagementInstanz2 = new DBManagement();
-
     DBManagementInstanz2.dbDateiUpdaten(1, 'belegt');
-    DBManagementInstanz2.dbDateiUpdaten(3, 'belegt');
-    DBManagementInstanz2.dbDateiUpdaten(5, 'belegt');
-    DBManagementInstanz2.dbDateiUpdaten(7, 'belegt');
-    DBManagementInstanz2.dbDateiUpdaten(9, 'belegt');
-
     res.send(DBManagementInstanz2.jsonDateiAuslesen('./DB/DB.json'));
 
 })
